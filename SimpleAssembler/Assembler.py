1e1267aee723917591114cb651ed5f14c30dde7c
manas25301-cyber <manas25301@iiitd.ac.in>
2026-03-02 18:33:02 +0530

    Coded I-type instruction and initialised code to handle labels

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -12,40 +12,49 @@ with open(path) as f:
         if i=="":
             continue
         i=i.split()
+        if len(i)!=2:
+            continue
         i[-1]=i[-1].split(",")
         cmd.append(i)
 
 instr={"R":["add","sub","sll","slt","sltu","xor","srl","or","and"],"I":["lw","addi","sltiu","jalr"],"S":["sw"],"B":["beq","bne","blt","bge","bltu","bgeu"],"U":["lui","auipc"],"J":["jal"]}
 
-for exe in cmd:
-    idx=0
-    for chk in list(instr.values()):
-        if exe[0] in chk:
-            break
-        else:
-            idx+=1
-    if idx==0:
-        print("R")
-        #execute R-type
-    elif idx==1:
-        print("I")
-        #execute I-type
-    elif idx==2:
-        print("S")
-        #execute S-type
-    elif idx==3:
-        print("B")
-        #execute B-type
-    elif idx==4:
-        print("U")
-        #execute U-type
-    elif idx==5:
-        print("J")
-        #execute J-type
-    else:
-        print("NA")
-        #error:cmd not found
-        
+#exe->Each executable, format->[instruction, [rd, rs1, rs2]] (Nested List)
+
+def main():
+    for exe in cmd:
+        idx=0
+        for chk in list(instr.values()):
+            if exe[0] in chk:
+                break
+            else:
+                idx+=1
+
+        if idx==0: #execute R-type
+            print("R")
+
+        elif idx==1: #execute I-type
+            if exe[0]=="lw":
+                tmp=exe[1][1]
+                imm, rs = tmp.replace(")", "").split("(")
+                print(I_type(exe[0], exe[1][0], rs, imm))
+            else:
+                print(I_type(exe[0], exe[1][0], exe[1][1], exe[1][2]))
+
+        elif idx==2: #execute S-type
+            print("S")         
+
+        elif idx==3: #execute B-type
+            print("B")    
+
+        elif idx==4: #execute U-type
+            print("U")
+            
+        elif idx==5: #execute J-type
+            print("J")
+            
+        else: #error:cmd not found
+            print("NA")                   
 
 def register(r):
     reg=['zero','ra','sp','gp','tp','t0','t1','t2','s0','s1','a0','a1','a2','a3','a4','a5','a6','a7','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','t3','t4','t5','t6']
@@ -58,6 +67,29 @@ def register(r):
     if c=='fp':
         b='01000'
     return b
+
+def I_type(ins, rd, rs, imm):
+    s=''
+    imm=format(int(imm) & 0xFF, '012b')
+    rd=register(rd)
+    rs=register(rs)
+    s=s+imm
+    if ins=="lw":
+        s=s+rs+"010"+rd+"0000011"
+    elif ins=="addi":
+        s=s+rs+"000"+rd+"0010011"
+    elif ins=="sltiu":
+        s=s+rs+"011"+rd+"0010011"
+    elif ins=="jalr":
+        s=s+rs+"000"+rd+"1100111"
+    else:
+        s="error"
+    return s
+
+main()
+
+
+
 
 
 
