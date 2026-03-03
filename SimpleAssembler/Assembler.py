0bfb17c86940f13d7ae962351c7ef093da60e785
Aditya Chowdhry <adityachowdhry2007@gmail.com>
2026-03-03 17:54:27 +0530

    Implemented S-type and U-type and J-type encoding

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -6,32 +6,61 @@ def register(r):
             b=f'{i:05b}'
             c=1
             break
-    if c==0:
+    if r=='fp':
         b='01000'
     return b
 
-def S_Type(key,rs2,s):
-    val= int(s[:s.index("(")])
-    rs1= s[s.index("(")+1 : -1]
-    val_12bit = format(val, '012b')
-    
-    rs2_B= register(rs2)
-    rs1_B= register(rs1)
-    
-    opcode="0100011"
-    
-    if key=="sb":
-        funct3="000"
-    elif key=="sh":
-        funct3="001"
-    elif key=="sw":
-        funct3="010"
-    
-    return(val_12bit[0:7]+rs2_B+rs1_B+funct3+val_12bit[7:]+opcode)
+def S_Type(key, rs2, s):
+    val = int(s[:s.index("(")])
+    rs1 = s[s.index("(")+1 : -1]
+
+    val_12bit = format(val & 0xFFF, '012b')
+
+    rs2_B = register(rs2)
+    rs1_B = register(rs1)
+
+    opcode = "0100011"
+
+    if key == "sb":
+        funct3 = "000"
+    elif key == "sh":
+        funct3 = "001"
+    elif key == "sw":
+        funct3 = "010"
+    else:
+        raise ValueError("Invalid S-type instruction")
+
+    return (val_12bit[0:7] + rs2_B + rs1_B + funct3 +
+            val_12bit[7:] + opcode)
 
 
-def U_Type():
-    return 0
-def J_Type():
-    return 0
+def U_Type(key,rd,imm):
+    imm_20bit = format(imm & 0xFFFFF, '020b')
+    rd_B= register(rd)
+    if key=="lui":
+        opcode="0110111"
+    elif key=="auipc":
+        opcode="0010111"
 
+    return(imm_20bit+rd_B+opcode)
+    
+def J_Type(key,rd,offset):
+    if offset % 2 != 0:
+        raise ValueError("Offset must be 2-byte aligned")
+
+    offset=offset//2
+
+    if offset<-(2**20) or offset>=(2**20):
+        raise ValueError("Offset out of 21-bit range")
+
+    imm = offset & 0x1FFFFF
+    offset_20bit = format(imm, '021b')
+
+    rd_B= register(rd)
+    opcode="1101111"
+
+    return(offset_20bit[0]+offset_20bit[10:20]+offset_20bit[9]+offset_20bit[1:9]+rd_B+opcode)
+
+print(S_Type("sw","s2","0(s0)"))
+print(U_Type("lui","ra",20))
+print(J_Type("jal","ra",20))
