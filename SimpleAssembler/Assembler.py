c4ad0b26f8711a48d4a572ea8ad7151952a625de
ayushman25148-dank <ayushman25148@iiitd.ac.in>
2026-03-04 16:37:36 +0530

    Add error handling to register function
    Refactor register function to include error handling for invalid register input.

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -67,16 +67,20 @@ def main():
             print("NA")                   
 
 def register(r):
-    reg=['zero','ra','sp','gp','tp','t0','t1','t2','s0','s1','a0','a1','a2','a3','a4','a5','a6','a7','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','t3','t4','t5','t6']
-    c=0
-    for i in range(32):
-        if reg[i]==r:
-            b=f'{i:05b}'
-            c=1
-            break
-    if r=='fp':
-        b='01000'
-    return b
+    try:
+        reg=['zero','ra','sp','gp','tp','t0','t1','t2','s0','s1','a0','a1','a2','a3','a4','a5','a6','a7','s2','s3','s4','s5'
+            ,'s6','s7','s8','s9','s10','s11','t3','t4','t5','t6']
+        c=0
+        for i in range(32):
+            if reg[i]==r:
+                b=f'{i:05b}'
+                c=1
+                break
+        if r=='fp':
+            b='01000'
+        return b
+    except:
+        raise ValueError("ERROR:Invalid Register Provided")
 
 def R_type(ins,rd,rs1,rs2):
         opcode="0110011"
