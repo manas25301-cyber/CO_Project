d7c356c045ae0de35a1d15c11832cefc8fb8a342
manas25301-cyber <manas25301@iiitd.ac.in>
2026-03-04 17:12:19 +0530

    Suitable for automated testing

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -1,11 +1,15 @@
 import sys
 
+input_file=sys.argv[1]
+output_file=sys.argv[2]
+
 PC=0
 cmd=[]
 labels=[]
-lines=sys.stdin.read().splitlines()
 
-for x in lines:
+with open(input_file) as f:
+    tmp=[]
+    x=f.read()
     tmp=x.split("\n")
     for i in tmp:
         if i=="":
@@ -24,7 +28,12 @@ instr={"R":["add","sub","sll","slt","sltu","xor","srl","or","and"],"I":["lw","ad
 
 #exe->Each executable, format->[instruction, [rd, rs1, rs2], PC] (Nested List)
 
+f=open(output_file, 'w')
+f.close() #Makes sure output file is empty
+
 def main():
+    f=open(output_file, 'a')
+    s=''
     for exe in cmd:
         idx=0
         for chk in list(instr.values()): #Checks Each List of the dictionary
@@ -34,24 +43,24 @@ def main():
                 idx+=1
 
         if idx==0: #execute R-type
-            print(R_type(exe[0], exe[1][0], exe[1][1], exe[1][2]))
+            s=R_type(exe[0], exe[1][0], exe[1][1], exe[1][2])
 
         elif idx==1: #execute I-type
             if exe[0]=="lw":
                 tmp=exe[1][1]
                 imm, rs = tmp.replace(")", "").split("(")
-                print(I_type(exe[0], exe[1][0], rs, imm))
+                s=I_type(exe[0], exe[1][0], rs, imm)
             else:
-                print(I_type(exe[0], exe[1][0], exe[1][1], exe[1][2]))
+                s=I_type(exe[0], exe[1][0], exe[1][1], exe[1][2])
 
         elif idx==2: #execute S-type
-            print(S_Type(exe[0], exe[1][0], exe[1][1]))         
+            s=S_Type(exe[0], exe[1][0], exe[1][1])       
 
         elif idx==3: #execute B-type
-            print(B_type(exe[0],exe[1][0],exe[1][1],exe[1][2],exe[-1]))    
+            s=B_type(exe[0],exe[1][0],exe[1][1],exe[1][2],exe[-1])    
 
         elif idx==4: #execute U-type
-            print(U_Type(exe[0], exe[1][0], exe[1][1]))
+            s=U_Type(exe[0], exe[1][0], exe[1][1])
             
         elif idx==5: #execute J-type
             currentpc = int(exe[-1], 16)
@@ -61,22 +70,28 @@ def main():
                     break
             label_program_counter = int(labels[j][1], 16)
             offset = label_program_counter - currentpc
-            print(J_Type(exe[0],exe[1][0],offset))
+            s=J_Type(exe[0],exe[1][0],offset)
             
         else: #error:cmd not found
-            print("NA")                   
+            print("NA")        
+        f.write(s)
+        f.write("\n")
+    f.close()          
 
 def register(r):
-    reg=['zero','ra','sp','gp','tp','t0','t1','t2','s0','s1','a0','a1','a2','a3','a4','a5','a6','a7','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','t3','t4','t5','t6']
-    c=0
-    for i in range(32):
-        if reg[i]==r:
-            b=f'{i:05b}'
-            c=1
-            break
-    if r=='fp':
-        b='01000'
-    return b
+    try:
+        reg=['zero','ra','sp','gp','tp','t0','t1','t2','s0','s1','a0','a1','a2','a3','a4','a5','a6','a7','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','t3','t4','t5','t6']
+        c=0
+        for i in range(32):
+            if reg[i]==r:
+                b=f'{i:05b}'
+                c=1
+                break
+        if r=='fp':
+            b='01000'
+        return b
+    except:
+        raise ValueError("ERROR: Invalid Register Provided")
 
 def R_type(ins,rd,rs1,rs2):
         opcode="0110011"
@@ -96,6 +111,8 @@ def R_type(ins,rd,rs1,rs2):
         return code
 
 def I_type(ins, rd, rs, imm):
+    if (imm<-2048 or imm>2047):
+        return "Immediate out of range"
     s=''
     imm=format(int(imm) & 0xFFF, '012b')
     rd=register(rd)
@@ -116,7 +133,8 @@ def I_type(ins, rd, rs, imm):
 def S_Type(key, rs2, s):
     val = int(s[:s.index("(")])
     rs1 = s[s.index("(")+1 : -1]
-
+    if val < -2048 or val > 2047:
+        raise ValueError("S-type immediate out of 12-bit signed range (-2048 to 2047)")
     val_12bit = format(val & 0xFFF, '012b')
 
     rs2_B = register(rs2)
@@ -163,13 +181,22 @@ def B_type(ins,r1,r2,imm,currentpc):
         label_program_counter = int(labels[j][1], 16)
         offset = label_program_counter - currentpc
         imm = offset
-    imm = int(imm)
+    try:
+        imm = int(imm)
+    except:
+        raise ValueError("ERROR: INVALID LABEL GIVEN")
+    if imm % 2 != 0:
+        raise ValueError("Branch offset must be multiple of 2")
+    if imm < -4096 or imm > 4094:
+        raise ValueError("Branch offset out of range")
     imm = imm // 2
     immcode = format(imm & 0xFFF, '012b')
     code = immcode[0] + immcode[2:8] + r2_ + r1_ + func3 + immcode[8:] + immcode[1] + opcode
     return code
 
 def U_Type(key,rd,imm):
+    if imm < -(2**19) or imm > (2**19)-1:
+        raise ValueError("U-type immediate out of 20-bit signed range")
     imm_20bit = format(imm & 0xFFFFF, '020b')
     rd_B= register(rd)
     if key=="lui":
@@ -180,6 +207,22 @@ def U_Type(key,rd,imm):
     return(imm_20bit+rd_B+opcode)
 
 def J_Type(key,rd,offset):
+    offset=(offset)
+    if offset % 2 != 0:
+        raise ValueError("Offset must be 2-byte aligned")
+
+    offset=offset//2
+
+    if offset<-(2**20) or offset>=(2**20):
+        raise ValueError("Offset out of 21-bit range")
+
+    imm = offset & 0x1FFFFF
+    offset_20bit = format(imm, '021b')
+
+    rd_B= register(rd)
+    opcode="1101111"
+
+    return(offset_20bit[0]+offset_20bit[10:20]+offset_20bit[9]+offset_20bit[1:9]+rd_B+opcode)
     offset=(offset)
     if offset % 2 != 0:
         raise ValueError("Offset must be 2-byte aligned")
